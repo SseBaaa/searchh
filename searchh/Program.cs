2c40ed2e1fca42a77e5a1707ef9856f4fce6eab7
sebastian krnjus <sebastiankrnjus25@gmail.com>
2022-10-16 22:18:28 +0200

    Add project files.

---
+++ searchh/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace searchh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Search ObjektSearch = new Search();
+            int[] arrSearch = new int[100];
+            int neparnibroj = 1;
+            for (int i = 0; i < 100; i++)
+            {
+                arrSearch[i] = neparnibroj;
+                neparnibroj += 2;
+            }
+            Console.WriteLine("Index broja 75 je:" + ObjektSearch.BinarySearch(arrSearch, 75));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ searchh/Search.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace searchh
+{
+    internal class Search
+    {
+        public int BinarySearch(int[] arr, int x)
+        {
+            int l = 0, r = arr.Length - 1;
+            while (l <= r)
+            {
+                int m = l + (r - l) / 2;
+                if (arr[m] == x)
+                    return m;
+                if (arr[m] < x)
+                    l = m + 1;
+                else
+                    r = m - 1;
+            }
+            return -1;
+        }
+    }
+}
